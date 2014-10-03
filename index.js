3b46af08eb434de532a7424f6b765e4a5b3f1436
Thomas Watson Steen <w@tson.dk>
2014-10-02 21:33:22 -0700

    Add exit message

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ var peerflix = require('peerflix')
 var network = require('network-address')
 var events = require('events')
 
+process.on('SIGINT', function() {
+  // we're doing some heavy lifting so it can take some time to exit... let's
+  // better output a status message so the user knows we're working on it :)
+  console.log('\nPeercast is exiting...')
+  process.exit()
+})
+
 module.exports = function(torrent, opts) {
   if (!opts) opts = {}
 
